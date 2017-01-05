a7a3e058d4470c0fbb41976e3fcbdc5ad3e48d8e
Benjamin Collins <admin@termantics.com>
2017-01-06 07:46:38 +0900

    added output image

--- 01_open_a_window/main.c
+++ 01_open_a_window/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
 	GtkApplication *app;
 	int status;
 
-	app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+	app = gtk_application_new("com.termantics.example1", G_APPLICATION_FLAGS_NONE);
 	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 	status = g_application_run(G_APPLICATION(app), argc, argv);
 	g_object_unref(app);
@@ -22,7 +22,7 @@ static void activate(GtkApplication *app, gpointer user_data) {
 
 	window = gtk_application_window_new(app);
 	gtk_window_set_title(GTK_WINDOW(window), "Window");
-	gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+	gtk_window_set_default_size(GTK_WINDOW(window), 640, 480);
 	gtk_widget_show_all(window);
 
 }
